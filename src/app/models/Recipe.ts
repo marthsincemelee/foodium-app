e6fe0ad9163473a37f58b6279c92a091352150cb
Felix Grohme <felix@grohme.de>
2022-02-12 00:07:39 +0100

    Added models

--- src/app/models/Recipe.ts
+++ src/app/models/Recipe.ts
@@ -4,13 +4,11 @@ export class Recipe {
   name: string;
   recipe_link:string;
   ingredients : Array<Ingredient>;
-  favourite: boolean;
 
 
-  constructor(name: string, recipe_link: string, ingredients: Array<Ingredient>, favourite: boolean) {
+  constructor(name: string, recipe_link: string, ingredients: Array<Ingredient>) {
     this.name = name;
     this.recipe_link = recipe_link;
     this.ingredients = ingredients;
-    this.favourite = favourite;
   }
 }
---
+++ src/app/models/user.ts
@@ -0,0 +1,22 @@
+import {Recipe} from "./Recipe";
+
+export class User {
+  id : number
+  username: string
+  email : string
+  provider: string
+  confirmed: boolean
+  blocked: boolean
+  recipes : Array<Recipe>
+
+
+  constructor(id: number, username: string, email: string, provider: string, confirmed: boolean, blocked: boolean, recipes: Array<Recipe>) {
+    this.id = id;
+    this.username = username;
+    this.email = email;
+    this.provider = provider;
+    this.confirmed = confirmed;
+    this.blocked = blocked;
+    this.recipes = recipes;
+  }
+}
